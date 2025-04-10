43acb2a2740e0544083f481435f99a4c892b25ed
Sumit Khandelwal <synergy2411@outlook.com>
2025-04-10 13:19:42 +0530

    user sign up added

--- 03-graphql-prisma-mongo/src/server.js
+++ 03-graphql-prisma-mongo/src/server.js
@@ -1,13 +1,64 @@
 import { createSchema, createYoga } from "graphql-yoga";
 import { createServer } from "node:http";
+import { PrismaClient } from "../prisma/app/generated/prisma/client/index.js";
+import { GraphQLError } from "graphql";
+
+import bcrypt from "bcrypt";
+
+const { hashSync } = bcrypt;
+
+const prisma = new PrismaClient();
 
 const typeDefs = /* GraphQL */ `
+  type Mutation {
+    signUp(data: SignUpInput!): SignUpPayload!
+  }
   type Query {
     hello: String!
+  }
+
+  type SignUpPayload {
+    message: String!
+  }
+
+  input SignUpInput {
+    name: String!
+    age: Int!
+    email: String!
+    password: String!
+    role: Role!
+  }
+
+  enum Role {
+    DEVELOPER
+    MANAGER
+    ADMIN
   }
 `;
 
 const resolvers = {
+  Mutation: {
+    signUp: async (parent, args, context, info) => {
+      try {
+        const { name, age, email, password, role } = args.data;
+        const hashedPassword = hashSync(password, 12);
+        const createdUser = await prisma.user.create({
+          data: {
+            name,
+            age,
+            email,
+            password: hashedPassword,
+            role,
+          },
+        });
+        console.log(createdUser);
+        return { message: "User created with Id - " + createdUser.id };
+      } catch (err) {
+        console.log(err);
+        throw new GraphQLError(err);
+      }
+    },
+  },
   Query: {
     hello: () => "Hello World",
   },
