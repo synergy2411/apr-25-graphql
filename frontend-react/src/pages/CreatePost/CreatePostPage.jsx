b49a5b5fe9f5ae94d2fb76d77da654fc4bc7cdc3
Sumit Khandelwal <synergy2411@outlook.com>
2025-04-11 14:56:33 +0530

    create post form added

--- frontend-react/src/pages/CreatePost/CreatePostPage.jsx
+++ frontend-react/src/pages/CreatePost/CreatePostPage.jsx
@@ -1,5 +1,56 @@
 function CreatePostPage() {
-  return <>Create post form coming soon...</>;
+  return (
+    <>
+      <div className="row">
+        <div className="col-8 offset-2">
+          <div className="card">
+            <div className="card-body">
+              <h3 className="text-center">Create Post</h3>
+              <form>
+                {/* title */}
+                <div className="form-floating mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    name="title"
+                    id="title"
+                    placeholder=""
+                  />
+                  <label htmlFor="title">Title:</label>
+                </div>
+
+                {/* body */}
+                <div className="mb-3">
+                  <label htmlFor="" className="form-label"></label>
+                  <textarea
+                    className="form-control"
+                    name=""
+                    id=""
+                    rows="3"
+                  ></textarea>
+                </div>
+
+                <div className="row">
+                  {/* button - submit */}
+                  <div className="col-6">
+                    <div className="d-grid">
+                      <button className="btn btn-primary">Create Post</button>
+                    </div>
+                  </div>
+                  {/* button - reset */}
+                  <div className="col-6">
+                    <div className="d-grid">
+                      <button className="btn btn-secondary">Reset</button>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreatePostPage;
