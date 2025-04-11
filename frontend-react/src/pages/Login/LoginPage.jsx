a4bdc527fb70387f236080a87ebee75b24a44397
Sumit Khandelwal <synergy2411@outlook.com>
2025-04-11 13:19:09 +0530

    token generated

--- frontend-react/src/pages/Login/LoginPage.jsx
+++ frontend-react/src/pages/Login/LoginPage.jsx
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+
+const USER_SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(data: { email: $email, password: $password }) {
+      token
+    }
+  }
+`;
+
 function LoginPage() {
+  const [enteredEmail, setEnteredEmail] = useState("");
+  const [enteredPassword, setEnteredPassword] = useState("");
+
+  const [signInMutation, { error, loading }] = useMutation(USER_SIGN_IN);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(enteredEmail, enteredPassword);
+    signInMutation({
+      variables: {
+        email: enteredEmail,
+        password: enteredPassword,
+      },
+    })
+      .then(console.log)
+      .catch(console.error);
+  };
+
   return (
     <>
-      <div className="row">
+      <div className="row mt-4">
         <div className="col-8 offset-2">
           <div className="card">
             <div className="card-body">
               <h3 className="text-center">Login Form</h3>
-              <form>
+              <form onSubmit={submitHandler}>
+                {error && <p className="alert alert-danger">{error.message}</p>}
                 {/* email */}
                 <div className="form-floating mb-3">
                   <input
@@ -15,6 +45,8 @@ function LoginPage() {
                     name="email"
                     id="email"
                     placeholder=""
+                    value={enteredEmail}
+                    onChange={(e) => setEnteredEmail(e.target.value)}
                   />
                   <label htmlFor="email">Email:</label>
                 </div>
@@ -27,6 +59,8 @@ function LoginPage() {
                     name="password"
                     id="password"
                     placeholder=""
+                    value={enteredPassword}
+                    onChange={(e) => setEnteredPassword(e.target.value)}
                   />
                   <label htmlFor="password">Password</label>
                 </div>
@@ -35,7 +69,13 @@ function LoginPage() {
                 <div className="row">
                   <div className="col-6">
                     <div className="d-grid">
-                      <button className="btn btn-primary">Login</button>
+                      <button
+                        className="btn btn-primary"
+                        type="submit"
+                        disabled={loading}
+                      >
+                        {loading ? "Submitting..." : "Login"}
+                      </button>
                     </div>
                   </div>
                   <div className="col-6">
